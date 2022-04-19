2b53f7684b548e4dba0598e9dd22028134106767
MinjinChae <eominjin25@gamil.com>
2022-04-19 13:46:05 +0900

    update articles

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
+from .models import Article
+from .forms import ArticleForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@require_safe
+def index(request):
+    articles = Article.objects.order_by('-pk')
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def create(request):
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            article.user = request.user
+            article.save()
+            return redirect('articles:detail', article.pk)
+        else:
+            form = ArticleForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'articles/create.html', context)
+
+@require_safe
+def detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.user.is_authenticated:
+        if request.user == article.user:
+            article.delete()
+    return redirect('articles:index')
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def update(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.user == article.user:
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:index', article.pk)
+        else:
+            form = ArticleForm(instance=article)
+    else:
+        return redirect('articles:index')
+    context = {
+        'article': article,
+        'form': form,
+    }
+    return render(request, 'articles/update.html', context)
